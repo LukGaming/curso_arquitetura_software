0707cad0427b27243f85993f216a350feb12d3ee
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2025-05-26 20:06:28 -0300

    Command 0 e Command 1

--- lib/utils/command.dart
+++ lib/utils/command.dart
@@ -1,6 +1,9 @@
 import 'package:curso_arquitetura_software/utils/result.dart';
 import 'package:flutter/material.dart';
 
+typedef CommandAction0<T> = Future<Result<T>> Function();
+typedef CommandAction1<T, P> = Future<Result<T>> Function(P params);
+
 abstract class Command<T> extends ChangeNotifier {
   bool _running = false;
   bool get running => _running;
@@ -11,7 +14,7 @@ abstract class Command<T> extends ChangeNotifier {
   bool get completed => _result is Ok;
   bool get error => _result is Error;
 
-  Future<void> _execute(Future<Result<T>> Function() action) async {
+  Future<void> _execute(CommandAction0<T> action) async {
     if (_running) return;
 
     _running = true;
@@ -23,5 +26,24 @@ abstract class Command<T> extends ChangeNotifier {
       _running = false;
       notifyListeners();
     }
+  }
+}
+
+class Command0<T> extends Command<T> {
+  final CommandAction0<T> action;
+
+  Command0(this.action);
+
+  Future<void> execute() async {
+    await _execute(action);
+  }
+}
+
+class Command1<T, P> extends Command<T> {
+  final CommandAction1<T, P> action;
+  Command1(this.action);
+
+  Future<void> execute(P params) async {
+    await _execute(() => action(params));
   }
 }
